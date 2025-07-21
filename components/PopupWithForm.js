f266212c3b04e9a9df900b7f99001acbabf81295
TamaraHeater <tamaraheater@gmail.com>
2025-07-20 21:32:54 -0500

    add counter file

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -3,15 +3,26 @@ import Popup from "./Popup.js";
 class PopupWithForm extends Popup {
   constructor({ popupSelector, handleFormSubmit }) {
     super({ popupSelector });
-    this._popupForm = this.popupElement.querySelector(".popup__form");
+    this._popupForm = this._popupElement.querySelector(".popup__form");
+    this._inputList = this._popupForm.querySelectorAll(".popup__input");
     this._handleFormSubmit = handleFormSubmit;
+  }
+
+  getInputValues() {
+    const values = {};
+    this._inputList.forEach((input) => {
+      values[input.name] = input.value;
+    });
+    return values;
   }
 
   setEventListeners() {
     super.setEventListeners();
     this._popupForm.addEventListner("submit", (evt) => {
       evt.preventDefault();
-      this._handleFormSubmit(evt);
+      const inputValues = this._getInputValues;
+      //pass result of _getInputValues to submission handler
+      this._handleFormSubmit(evt, values);
     });
   }
 }
