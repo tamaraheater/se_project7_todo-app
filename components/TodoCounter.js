f2283eadb1e3a72e137e57dcd5c3007ba2d8a34d
TamaraHeater <tamaraheater@gmail.com>
2025-07-25 23:17:13 -0500

    comments removed

--- components/TodoCounter.js
+++ components/TodoCounter.js
@@ -1,6 +1,4 @@
 class TodoCounter {
-  // todos should be the array of initial todos
-  // selector is the selector for the counter text element
   constructor(todos, selector) {
     this._element = document.querySelector(selector);
     this._element.textContent = `Showing ${this._completed} out of ${this._total} completed`;
